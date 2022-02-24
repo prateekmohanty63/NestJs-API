cfce9ac1957cc45e7415cb4e2f210db755a93da3
Prateek <prateekmohanty63@gmail.com>
2022-02-24 13:49:46 +0530

    Feat(Item controller) Added Fetch and Post methods

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,16 +1,34 @@
-import { Controller,Get,Post,Put,Delete} from '@nestjs/common';
+import { Controller,Get,Post,Put,Delete,Body,Req,Res,Param} from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import {Request,Response} from 'express'
 
 @Controller('items')
 export class ItemsController {
 
+    // ONE Method of accessing request and response object in Nestjs
+    // @Get()
+    // findAll(@Req() req:Request,@Res() res:Response):Response{
+    //     console.log(req.url)
+    //     return res.send('Hello World')
+    //     //return 'Get all items';
+    // }
+
+    // Fetch all items
     @Get()
-    findAll():string{
+    findAll():String{
         return 'Get all items';
     }
 
+    // Fetch particluar item
+    @Get(':id')
+    findOne(@Param() param){
+       return `Item ${param.id}`
+    }
+
+    // Post item
     @Post()
-    create():string{
-        return 'Created item'
+    create(@Body() createItemDto:CreateItemDto):string{
+        return `Name:${createItemDto.name} Desc:${createItemDto.description}`
     }
 }
 
