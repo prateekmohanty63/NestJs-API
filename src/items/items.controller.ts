c4bef019ff14430859b34dd5cacf26034ec4acaf
Prateek <prateekmohanty63@gmail.com>
2022-02-24 13:36:34 +0530

    Added Items class

---
+++ src/items/items.controller.ts
@@ -0,0 +1,17 @@
+import { Controller,Get,Post,Put,Delete} from '@nestjs/common';
+
+@Controller('items')
+export class ItemsController {
+
+    @Get()
+    findAll():string{
+        return 'Get all items';
+    }
+
+    @Post()
+    create():string{
+        return 'Created item'
+    }
+}
+
+
